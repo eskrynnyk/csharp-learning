076d23e98720b8b1c9b2b51d6a355cef27fcef23
Evgenii <evgeniy.skrynnik@acumatica.com>
2025-11-10 14:23:39 -0500

    Initial commit: Add C# learning project with Chapter 012

---
+++ for_dot_NET/Lessons/012_chapter/Program_012.cs
@@ -0,0 +1,38 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Строгая типизация, объявление переменных
+int age; // Объявление переменной типа int с именем age
+double salary; // Объявление переменной типа double с именем salary
+string name; // Объявление переменной типа string с именем name
+
+// Присваивание значений
+age = 30; // Присваивание значения 30 переменной age
+salary = 0002500.50000; // Присваивание значения 2500.50 переменной salary
+name = "John"; // Присваивание значения "John" переменной name
+
+// Инициализация переменных
+string productName = "Ноутбук";
+char c = 'A';
+double price = 1200.99;
+int quantityInStock = 50;
+productName = "Телефон";
+string acumaticaOrderID = "SO0001";
+int acumaticaOrderNbr = 101;
+
+// Чувствительность к регистру
+// productName - не равно ProductName
+
+Console.WriteLine("Какой-то текст");
+Console.WriteLine("Наименование товара " + productName);
+Console.WriteLine("Ценник " + price);
+Console.WriteLine("Sales Order " + acumaticaOrderID + " equal " + acumaticaOrderNbr);
+
+//Task
+double x = 3;
+double y = 4;
+double z = 5;
+double result = x * y * z;
+Console.WriteLine(result);
+
